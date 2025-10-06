1594d68d063e215ac983ae8ecf9b613d021c37ae
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-06 09:40:28 -0300

    commita ai

--- src/pages/pagesExtra/quemSomos/QuemSomos.js
+++ src/pages/pagesExtra/quemSomos/QuemSomos.js
@@ -11,17 +11,17 @@ export default function Clubes() {
         <Navbar />
       </nav>
       <div className={styles.containerFoto}>
-        <Image src="/fotoNossa.png" width={1120} height={510} />
+        <Image src="/fotoNossa.png" width={1120} height={510} alt="iamgem-fotoNossa" />
       </div>
       <div>
         <p className={styles.paragrafo}>lorem</p>
         <div className={styles.fotoSeparadas}>
-          <Image src="/fotoCaio.png" width={300} height={300} />
-          <Image src="/fotoEdu.png" width={300} height={300} />
-          <Image src="/fotoJao.png" width={300} height={300} />
-          <Image src="/fotoMat.png" width={300} height={300} />
-          <Image src="/fotoNat.png" width={300} height={300} />
-          <Image src="/fotoKau.png" width={300} height={300} />
+          <Image src="/fotoCaio.png" width={300} height={300} alt="imagem-caio" />
+          <Image src="/fotoEdu.png" width={300} height={300} alt="imagem-edu"/>
+          <Image src="/fotoJao.png" width={300} height={300} alt="imagem-jao"/>
+          <Image src="/fotoMat.png" width={300} height={300} alt="imagem-mat" />
+          <Image src="/fotoNat.png" width={300} height={300} alt="imagem-nat"/>
+          <Image src="/fotoKau.png" width={300} height={300} alt="imagem-kauan"/>
         </div>
       </div>
       <footer>
